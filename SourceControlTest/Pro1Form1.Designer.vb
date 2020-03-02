4578204144567688aa4178f1749a0341ed9c5e36
Daniel Goss <gosshawk2@yahoo.co.uk>
2020-03-02 18:14:25 +0000

    v1.0.0.1 02-03-2020 18:13 - Changed title and filename of Form1 of project1

---
+++ SourceControlTest/Pro1Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPro1Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnProject1 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnProject1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 59)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(789, 143)
+        Me.Panel1.TabIndex = 0
+        '
+        'btnProject1
+        '
+        Me.btnProject1.Location = New System.Drawing.Point(347, 19)
+        Me.btnProject1.Name = "btnProject1"
+        Me.btnProject1.Size = New System.Drawing.Size(75, 23)
+        Me.btnProject1.TabIndex = 0
+        Me.btnProject1.Text = "Project1"
+        Me.btnProject1.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Location = New System.Drawing.Point(12, 2)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(789, 51)
+        Me.Panel2.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(3, 10)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(783, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'frmPro1Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 214)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "frmPro1Form1"
+        Me.Text = "Project 1 Form 1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnProject1 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ SourceControlTest/Pro1Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmPro1Form1
+    Private Sub btnProject1_Click(sender As Object, e As EventArgs) Handles btnProject1.Click
+        MsgBox("Project1")
+    End Sub
+End Class
